22b4d04a510963508309f443b07880d8a708663d
Fox <Fox@Foxs-MacBook.local>
2014-02-26 13:50:29 +0000

    checking to see why articles are displaying

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -1,24 +1,20 @@
 # myapp.rb
 require 'sinatra'
 
-article_filename = 'content' + '/index.html'
-article_content = File.read(article_filename)
-
 get '/' do
+  article_filename = 'content' + '/index.html'
+  article_content = File.read(article_filename)
   erb article_content
 end
 
 def article_exists?(request_path)
-  exists_filename = 'content' + request_path
-  exists_content = File.read(exists_filename)
   File.exists?('content' + request_path)
-  get 'request_path' do
-    erb exists_content
-  end
 end
 
-def call(env)
-  if article_exists?(env['REQUEST_PATH'])
-    [[article_exists?(env['REQUEST_PATH'])]]
-  end
+get '/:page' do
+  pass params[:page] = article_exists?
+  article_filename = 'content'
+  article_content = File.read(article_filename)
+  erb article_content
 end
+
