f136256a1dd439da24bb3146f0a255ca93523b93
Rosa Fox <rosafox89@gmail.com>
2014-02-27 10:17:23 +0000

    Tidied up the Sinatra_app

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -7,21 +7,18 @@ get '/' do
   erb article_content
 end
 
-#Checks that article exists... needs an argument to be fed into it (:page)
 def article_exists?(request_path)
   File.exists? local_path(request_path)
 end
 
 get '/:page' do
-  #Need to check that the file exists in the directory by calling article_exists?
   page = params[:page]
   pass unless article_exists?(page)
-  #If the file exists then render it... not sure if symbol will convert to a string for the filepath...:
   exists_filename = local_path(page)
   exists_content = File.read(exists_filename)
   erb exists_content
 end
 
 def local_path(file_name)
-   'content/' + file_name 
+  'content/' + file_name
 end
