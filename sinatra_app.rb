e3ee00d8123ebae333aff4f16a5a7556d4ba3bf7
Najaf Ali <ali.najaf@gmail.com>
2014-03-04 12:03:14 +0000

    aligns parameters to Mail delivery_method method invocation

--- sinatra_app.rb
+++ sinatra_app.rb
@@ -5,12 +5,12 @@ require 'mail'
 
 Mail.defaults do
   delivery_method :smtp,
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    domain: 'heroku.com',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    enable_starttls_auto: true
+                  address: 'smtp.sendgrid.net',
+                  port: '587',
+                  domain: 'heroku.com',
+                  user_name: ENV['SENDGRID_USERNAME'],
+                  password: ENV['SENDGRID_PASSWORD'],
+                  enable_starttls_auto: true
 end
 
 def article_exists?(request_path)
